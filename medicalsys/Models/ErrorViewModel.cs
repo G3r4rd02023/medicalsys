bc2ad5d7f829912e114e903560c217cfb5f455f3
Gerardo Lanza <60949087+glanza007@users.noreply.github.com>
2021-10-30 12:38:41 -0600

    First Commit

---
+++ medicalsys/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace medicalsys.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
